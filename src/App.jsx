e77db26a1c2eb0c8268791986880674512aae169
bikram <bikram.1795@gmail.com>
2021-08-04 13:01:34 +0530

    initialised vite, tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="h-screen w-full bg-gray-500">
+    </div>
+  )
+}
+
+export default App
